9890835222231bd1dc0c2054640005c57a47f397
Irasp11 <117228523+Irasp11@users.noreply.github.com>
2026-01-07 13:36:44 +0300

    Add files via upload

---
+++ test_item.py
@@ -0,0 +1,17 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_guest_should_see_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    # Нужно для визуальной проверки языка (по заданию)
+    time.sleep(30)
+
+    add_to_basket_button = browser.find_element(
+        By.CSS_SELECTOR, "button.btn-add-to-basket"
+    )
+
+    assert add_to_basket_button.is_displayed(), \
+        "Add to basket button is not displayed"
